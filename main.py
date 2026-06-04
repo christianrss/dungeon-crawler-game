1979cdcf8b2a1f2f2b7cad4457436dce23723d60
Christian Rafael <christianparadix@gmail.com>
2026-06-04 11:09:18 -0300

    feat: Creating the Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+from character import Character
 
 pygame.init()
 
@@ -7,12 +8,32 @@ screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGH
 
 pygame.display.set_caption("Dungeon Crawler")
 
+#create player
+player = Character(100, 100)
+
 # main game loop
 run = True
 while run:
+
+    #draw player on screen
+    player.draw(screen)
+
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+        #take keyboard presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                print("left")
+            if event.key == pygame.K_d:
+                print("right")
+            if event.key == pygame.K_w:
+                print("up")
+            if event.key == pygame.K_s:
+                print("down")
+
+    pygame.display.update()
         
 pygame.quit()
