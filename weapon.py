150a8f577208003ff27a15eaeb4c1188abe1ff48
Christian Rafael <christianparadix@gmail.com>
2026-07-30 21:17:39 -0300

    feat: Creating Enemies

--- weapon.py
+++ weapon.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 import constants
 
 class Weapon():
@@ -51,7 +52,7 @@ class Arrow(pygame.sprite.Sprite):
         self.dx = math.cos(math.radians(self.angle)) * constants.ARROW_SPEED
         self.dy = -math.sin(math.radians(self.angle)) * constants.ARROW_SPEED#-ve because pygame y coordinates increases down the screen
 
-    def update(self):
+    def update(self, enemy_list):
         #reposition based on speed
         self.rect.x += self.dx
         self.rect.y += self.dy
@@ -60,6 +61,13 @@ class Arrow(pygame.sprite.Sprite):
         if self.rect.right < 0 or self.rect.left > constants.SCREEN_WIDTH or self.rect.bottom < 0 or self.rect.top > constants.SCREEN_HEIGHT:
             self.kill()
 
+        #check collision between arrow and enemies
+        for enemy in enemy_list:
+            if enemy.rect.colliderect(self.rect) and enemy.alive:
+                damage = 10 + random.randint(-5, 5)
+                enemy.health -= damage
+                self.kill()
+                break
 
     def draw(self, surface):
         surface.blit(self.image, ((self.rect.centerx - int(self.image.get_width()/2)), self.rect.centery - int(self.image.get_height()/2)))
