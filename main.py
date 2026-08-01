a7b8de81b05460626661edcfdd5a33038986db36
Christian Rafael <christianparadix@gmail.com>
2026-08-01 14:25:52 -0300

    feat: Player Health Display

--- main.py
+++ main.py
@@ -26,6 +26,11 @@ def scale_img(image, scale):
     h = image.get_height()
     return pygame.transform.scale(image, (w * scale, h * scale))
 
+#load heart images
+heart_empty = scale_img(pygame.image.load("assets/images/items/heart_empty.png").convert_alpha(), constants.ITEM_SCALE)
+heart_half = scale_img(pygame.image.load("assets/images/items/heart_half.png").convert_alpha(), constants.ITEM_SCALE)
+heart_full = scale_img(pygame.image.load("assets/images/items/heart_full.png").convert_alpha(), constants.ITEM_SCALE)
+
 #load weapon images
 bow_image = scale_img(pygame.image.load("assets/images/weapons/bow.png").convert_alpha(), constants.WEAPON_SCALE)
 arrow_image = scale_img(pygame.image.load("assets/images/weapons/arrow.png").convert_alpha(), constants.WEAPON_SCALE)
@@ -48,6 +53,21 @@ for mob in mob_types:
         animation_list.append(temp_list)
     mob_animations.append(animation_list)
 
+#function for displaying game info
+def draw_info():
+    pygame.draw.rect(screen, constants.PANEL, (0, 0, constants.SCREEN_WIDTH, 50))
+    pygame.draw.line(screen, constants.WHITE, (0, 50), (constants.SCREEN_WIDTH, 50))
+    #draw lives
+    half_heart_drawn = False
+    for i in range(5):
+        if player.health >= ((i + 1) * 20):
+            screen.blit(heart_full, (10 + i *  50, 0))
+        elif (player.health % 20 > 0) and half_heart_drawn == False:
+            screen.blit(heart_half, (10 + i *  50, 0))
+            half_heart_drawn = True
+        else:
+            screen.blit(heart_empty, (10 + i * 50, 0))
+
 #damage text class
 class DamageText(pygame.sprite.Sprite):
     def __init__(self, x, y, damage, color):
@@ -67,7 +87,7 @@ class DamageText(pygame.sprite.Sprite):
 
 
 #create player
-player = Character(100, 100, 100, mob_animations, 0)
+player = Character(100, 100, 70, mob_animations, 0)
 
 #create enemy
 enemy = Character(200, 300, 100, mob_animations, 1)
@@ -132,8 +152,7 @@ while run:
     for arrow in arrow_group:
         arrow.draw(screen)
     damage_text_group.draw(screen)
-
-    print(enemy.health)
+    draw_info()
 
     #event handler
     for event in pygame.event.get():
